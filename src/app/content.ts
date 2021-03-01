9da36db980180fc9aec4105271f0c8ddd1ba588b
Gary Tou <gary@garytou.com>
2021-02-28 21:10:47 -0800

    recheck url on SPA page change

--- src/app/content.ts
+++ src/app/content.ts
@@ -7,73 +7,53 @@ import expensifyReport from "./content/expensifyReport";
 import svbBillPayAddIndivHaveBank from "./content/svbBillPayAddIndivHaveBank";
 import svbBillPayAddPayeeActivationCode from "./content/svbBillPayAddPayeeActivationCode";
 
-chrome.runtime.sendMessage({}, (response) => {
-	var checkReady = setInterval(() => {
-		if (document.readyState === "complete") {
-			clearInterval(checkReady);
+function checkPath() {
+	chrome.runtime.sendMessage({}, (response) => {
+		var checkReady = setInterval(() => {
+			if (document.readyState === "complete") {
+				clearInterval(checkReady);
 
-			// match path to content function
-			const matches = [
-				{
-					regex: /https:\/\/bank\.hackclub\.com\/g_suites/,
-					func: bankGoogleWorkspace,
-				},
-				{
-					regex: /https:\/\/bank\.hackclub\.com\/.*\/g_suites\/.*\/edit/,
-					func: bankGoogleWorkspaceEdit,
-				},
-				{
-					regex: /https:\/\/www\.businessbillpay-e\.com\/V2\/Payees\/AddIndividual\.aspx.*/,
-					func: svbBillPayAddIndivHaveBank,
-				},
-				{
-					regex: /https:\/\/www\.businessbillpay-e\.com\/V2\/Payees\/ActivationCode\.aspx.*/,
-					func: svbBillPayAddPayeeActivationCode,
-				},
-				{
-					regex: /https:\/\/bank\.hackclub\.com\/transactions\/.*\/edit/,
-					func: bankTransactionEdit,
-				},
-				{
-					regex: /https:\/\/bank\.hackclub\.com\/.*/,
-					func: bankEventCopyName,
-				},
-				{
-					regex: /https:\/\/bank\.hackclub\.com\/events.*[?&]name=.*/,
-					func: bankProjectSearch,
-				},
-				{
-					regex: /https:\/\/.*expensify\.com\/report.*/,
-					func: expensifyReport,
-				},
-			];
+				// match path to content function
+				const matches = [
+					{
+						regex: /https:\/\/bank\.hackclub\.com\/g_suites/,
+						func: bankGoogleWorkspace,
+					},
+					{
+						regex: /https:\/\/bank\.hackclub\.com\/.*\/g_suites\/.*\/edit/,
+						func: bankGoogleWorkspaceEdit,
+					},
+					{
+						regex: /https:\/\/www\.businessbillpay-e\.com\/V2\/Payees\/AddIndividual\.aspx.*/,
+						func: svbBillPayAddIndivHaveBank,
+					},
+					{
+						regex: /https:\/\/www\.businessbillpay-e\.com\/V2\/Payees\/ActivationCode\.aspx.*/,
+						func: svbBillPayAddPayeeActivationCode,
+					},
+					{
+						regex: /https:\/\/bank\.hackclub\.com\/transactions\/.*\/edit/,
+						func: bankTransactionEdit,
+					},
+					{
+						regex: /https:\/\/bank\.hackclub\.com\/.*/,
+						func: bankEventCopyName,
+					},
+					{
+						regex: /https:\/\/bank\.hackclub\.com\/events.*[?&]name=.*/,
+						func: bankProjectSearch,
+					},
+					{
+						regex: /https:\/\/.*expensify\.com\/report.*/,
+						func: expensifyReport,
+					},
+				];
 
-			const url = window.location.href;
-			var matchesSpecificContent = false;
-			for (let item of matches) {
-				if (item.regex instanceof RegExp) {
-					if (url.match(item.regex)) {
-						matchesSpecificContent = true;
-						console.log("Hack Club Bank Ops Plugin is running on this page!");
-
-						// inject common css/scripts into page
-						injectCommon();
-
-						// run content specific function
-						item.func();
-					}
-				} else if (Array.isArray(item.regex)) {
-					var matched = false;
-					interface matchObj {
-						regex: RegExp;
-						func: Function;
-					}
-					(<Array<matchObj>>item.regex).forEach((r) => {
-						if (url.match(r.regex)) {
-							// don't run same function multiple times per page
-							if (matched) {
-								return;
-							}
+				const url = window.location.href;
+				var matchesSpecificContent = false;
+				for (let item of matches) {
+					if (item.regex instanceof RegExp) {
+						if (url.match(item.regex)) {
 							matchesSpecificContent = true;
 							console.log("Hack Club Bank Ops Plugin is running on this page!");
 
@@ -82,20 +62,62 @@ chrome.runtime.sendMessage({}, (response) => {
 
 							// run content specific function
 							item.func();
-
-							matched = true;
 						}
-					});
+					} else if (Array.isArray(item.regex)) {
+						var matched = false;
+						interface matchObj {
+							regex: RegExp;
+							func: Function;
+						}
+						(<Array<matchObj>>item.regex).forEach((r) => {
+							if (url.match(r.regex)) {
+								// don't run same function multiple times per page
+								if (matched) {
+									return;
+								}
+								matchesSpecificContent = true;
+								console.log(
+									"Hack Club Bank Ops Plugin is running on this page!"
+								);
+
+								// inject common css/scripts into page
+								injectCommon();
+
+								// run content specific function
+								item.func();
+
+								matched = true;
+							}
+						});
+					}
+				}
+				if (!matchesSpecificContent) {
+					console.log(
+						"Hack Club Bank Ops Plugin is installed, but not active on this page."
+					);
 				}
 			}
-			if (!matchesSpecificContent) {
-				console.log(
-					"Hack Club Bank Ops Plugin is installed, but not active on this page."
-				);
-			}
-		}
+		});
 	});
-});
+}
+checkPath();
+
+// check path on SPA page change
+let url = window.location.href;
+["click", "popstate", "onload"].forEach((evt) =>
+	window.addEventListener(
+		evt,
+		function () {
+			requestAnimationFrame(() => {
+				if (url !== location.href) {
+					checkPath();
+				}
+				url = location.href;
+			});
+		},
+		true
+	)
+);
 
 function injectCommon() {
 	const customCss = document.createElement("style");
--- src/app/content/expensifyReport.ts
+++ src/app/content/expensifyReport.ts
@@ -4,36 +4,41 @@ function expensifyReport() {
 function linkBankProjectSearch() {
 	const injectScript = document.createElement("script");
 	injectScript.innerText = `
-	
-	let observer = new MutationObserver((mutations) => {
-		const displayLoc = document.querySelector(
-			"#report_invoice_dates_container"
-			);
-			if (displayLoc === null) {
-				return;
-			}
-		const policyName = Policy.getCurrent().policy.name;
+	function inject() {
+		let observer = new MutationObserver((mutations) => {
+			const displayLoc = document.querySelector(
+				"#report_invoice_dates_container"
+				);
+				if (displayLoc === null) {
+					return;
+				}
+			const policyName = Policy.getCurrent().policy.name;
 			
-		const displayElem = document.createElement("div");
-		displayElem.classList.add("hcb-plugin-tools");
-		displayElem.innerHTML = \`
-			<p>
-				Search for
-				<a href='https://bank.hackclub.com/events?name=\` + policyName + \`' target='_blank'>\` + policyName + \`</a>
-				on Hack Club Bank.
-			</p>
-		\`;
-		displayLoc.appendChild(displayElem);
+			if(document.querySelector("#linkToHCBSearch") === null) {
+				const displayElem = document.createElement("div");
+				displayElem.classList.add("hcb-plugin-tools");
+				displayElem.id = "linkToHCBSearch";
+				displayElem.innerHTML = \`
+					<p>
+						Search for
+						<a href='https://bank.hackclub.com/events?name=\` + policyName + \`' target='_blank'>\` + policyName + \`</a>
+						on Hack Club Bank.
+					</p>
+				\`;
+				displayLoc.appendChild(displayElem);
+			}
 
-		observer.disconnect();
-	});
+			observer.disconnect();
+		});
 
-	observer.observe(document.body, {
-		childList: true,
-		subtree: true,
-		attributes: false,
-		characterData: false,
-	});
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true,
+			attributes: false,
+			characterData: false,
+		});
+	}
+	inject();
 	`;
 	document.head.appendChild(injectScript);
 }
